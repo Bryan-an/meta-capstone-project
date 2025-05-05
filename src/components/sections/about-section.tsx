9b43ea84a4d3c8b1798d997f4a129c132ed30e95
Bryan Andagoya <bryanandagoya@gmail.com>
2025-05-04 20:17:30 -0500

    refactor: update AboutSection layout and styles for improved responsiveness and visual consistency, enhance text and image presentation

--- src/components/sections/about-section.tsx
+++ src/components/sections/about-section.tsx
@@ -6,37 +6,24 @@ import { getTranslations } from 'next-intl/server';
  */
 export async function AboutSection() {
   const t = await getTranslations('About');
+
   return (
     <section className="px-4 py-16 md:px-8 lg:px-16">
-      <div className="container mx-auto grid items-center gap-16 md:grid-cols-2">
+      <div className="container mx-auto grid max-w-6xl items-center gap-8 sm:gap-16 md:grid-cols-2">
         {/* Text Content */}
         <div className="space-y-4">
-          <h2 className="text-primary-dark font-display text-6xl font-bold">
-            {t('title')}
-          </h2>
-          <h3 className="font-regular font-display text-4xl text-gray-700">
-            {t('subtitle')}
-          </h3>
-          <p className="font-body text-lg text-gray-800">{t('description1')}</p>
-          <p className="font-body text-lg text-gray-800">{t('description2')}</p>
+          <h2 className="text-primary text-6xl font-bold">{t('title')}</h2>
+          <h3 className="text-4xl text-gray-700">{t('subtitle')}</h3>
+          <p className="text-lg text-gray-800">{t('description1')}</p>
+          <p className="text-lg text-gray-800">{t('description2')}</p>
         </div>
 
         {/* Image Content */}
-        <div className="relative h-96">
-          <div className="absolute top-0 right-10 z-10 h-80 w-64 overflow-hidden rounded-lg lg:h-96 lg:w-72">
+        <div className="h-96">
+          <div className="relative h-80 w-full overflow-hidden rounded-lg lg:h-96 lg:w-full">
             <Image
               src="/images/mario-and-adrian-a.webp"
               alt={t('imageAlt1')}
-              fill
-              className="object-cover"
-              sizes="(max-width: 768px) 50vw, 33vw"
-              loading="lazy"
-            />
-          </div>
-          <div className="absolute bottom-0 left-0 h-80 w-64 overflow-hidden rounded-lg lg:h-96 lg:w-72">
-            <Image
-              src="/images/mario-and-adrian-b.webp"
-              alt={t('imageAlt2')}
               fill
               className="object-cover"
               sizes="(max-width: 768px) 50vw, 33vw"
